f053df8745c1402a8e78e5eadf815c282052c533
Fraser Tooth <frasertooth@gmail.com>
2024-06-04 12:07:45 +0900

    feat: more carbon tests and a restructure

--- denki-carbon/spec/carbon.spec.ts
+++ denki-carbon/spec/carbon.spec.ts
@@ -3,26 +3,43 @@ import {
   EXAMPLE_AREA_DATA_PROCESSED_ROW_TEPCO,
   EXAMPLE_AREA_DATA_PROCESSED_ROW_TEPCO_ALL_ZEROS,
   EXAMPLE_AREA_DATA_PROCESSED_ROW_TEPCO_OLD_FORMAT,
+  EXAMPLE_AREA_DATA_PROCESSED_ROW_TEPCO_OLD_FORMAT_ALL_ZEROS,
+  EXAMPLE_AREA_DATA_PROCESSED_ROW_TEPCO_OLD_FORMAT_ZERO_FOSSILS,
 } from "./fixtures";
 import { getTotalCarbonIntensityForAreaDataRow } from "../src/carbon";
-import exp from "constants";
 
 describe("carbon calculations", () => {
-  test("should calculate carbon intensity", () => {
-    const row = EXAMPLE_AREA_DATA_PROCESSED_ROW_TEPCO;
-    const carbonIntensity = getTotalCarbonIntensityForAreaDataRow(row);
-    expect(carbonIntensity).toBe(520.119);
+  describe("new format", () => {
+    test("should calculate carbon intensity", () => {
+      const row = EXAMPLE_AREA_DATA_PROCESSED_ROW_TEPCO;
+      const carbonIntensity = getTotalCarbonIntensityForAreaDataRow(row);
+      expect(carbonIntensity).toBe(520.119);
+    });
+
+    test("should calculate carbon intensity - all zeros", () => {
+      const row = EXAMPLE_AREA_DATA_PROCESSED_ROW_TEPCO_ALL_ZEROS;
+      const carbonIntensity = getTotalCarbonIntensityForAreaDataRow(row);
+      expect(carbonIntensity).toBe(0);
+    });
   });
 
-  test("should calculate carbon intensity - old format", () => {
-    const row = EXAMPLE_AREA_DATA_PROCESSED_ROW_TEPCO_OLD_FORMAT;
-    const carbonIntensity = getTotalCarbonIntensityForAreaDataRow(row);
-    expect(carbonIntensity).toBe(541.981);
-  });
+  describe("old format", () => {
+    test("should calculate carbon intensity", () => {
+      const row = EXAMPLE_AREA_DATA_PROCESSED_ROW_TEPCO_OLD_FORMAT;
+      const carbonIntensity = getTotalCarbonIntensityForAreaDataRow(row);
+      expect(carbonIntensity).toBe(541.981);
+    });
 
-  test("should calculate carbon intensity - all zeros", () => {
-    const row = EXAMPLE_AREA_DATA_PROCESSED_ROW_TEPCO_ALL_ZEROS;
-    const carbonIntensity = getTotalCarbonIntensityForAreaDataRow(row);
-    expect(carbonIntensity).toBe(0);
+    test("should calculate carbon intensity - all zeros", () => {
+      const row = EXAMPLE_AREA_DATA_PROCESSED_ROW_TEPCO_OLD_FORMAT_ALL_ZEROS;
+      const carbonIntensity = getTotalCarbonIntensityForAreaDataRow(row);
+      expect(carbonIntensity).toBe(0);
+    });
+
+    test("should calculate carbon intensity - zero fossils", () => {
+      const row = EXAMPLE_AREA_DATA_PROCESSED_ROW_TEPCO_OLD_FORMAT_ZERO_FOSSILS;
+      const carbonIntensity = getTotalCarbonIntensityForAreaDataRow(row);
+      expect(carbonIntensity).toBe(39.845);
+    });
   });
 });
