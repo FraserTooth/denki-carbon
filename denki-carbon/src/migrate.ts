2501c0c2377bd76357aa4933241a684183b1b842
Fraser Tooth <frasertooth@gmail.com>
2024-06-10 11:55:02 +0900

    chore: better migration script

--- denki-carbon/src/migrate.ts
+++ denki-carbon/src/migrate.ts
@@ -2,7 +2,19 @@ import { migrate } from "drizzle-orm/node-postgres/migrator";
 import { db, client } from "./db";
 
 console.log("Migrating database...");
-await migrate(db, { migrationsFolder: "./drizzle" });
-console.log("Database migrated successfully");
-
-await client.end();
+try {
+  await migrate(db, { migrationsFolder: "./drizzle" });
+  console.log("Database migrated successfully");
+  await client.end();
+  process.exit(0);
+} catch (e) {
+  console.log("Error migrating database");
+  console.log(e);
+  console.log(
+    process.env.DATABASE_URL.slice(0, 8) +
+      "********" +
+      process.env.DATABASE_URL.slice(-8)
+  );
+  await client.end();
+  process.exit(1);
+}
